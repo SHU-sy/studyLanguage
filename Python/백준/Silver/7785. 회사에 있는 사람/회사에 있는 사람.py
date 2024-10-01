41a6c54854736ba647b3eb2fc602dca25888538f
SHU <jisu.kimjss@gmail.com>
2024-10-01 16:38:38 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 152 ms, Memory: 42988 KB -BaekjoonHub

---
+++ Python/백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,13 @@
+from collections import deque
+import sys
+
+n = int(sys.stdin.readline().strip())
+p = set()
+for i in range(n):
+    a, b = sys.stdin.readline().split()
+    if b == 'enter':
+        p.add(a)
+    elif b == 'leave':
+        p.discard(a)
+
+print("\n".join(sorted(p, reverse=True)))
