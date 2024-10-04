1f1ec938f7ed067f19fdbb7ae9fbfd71b65cd561
SHU <jisu.kimjss@gmail.com>
2024-10-05 02:16:39 +0900

    [Gold III] Title: 나머지 합, Time: 632 ms, Memory: 142240 KB -BaekjoonHub

--- Python/백준/Gold/10986. 나머지 합/나머지 합.py
+++ Python/백준/Gold/10986. 나머지 합/나머지 합.py
@@ -3,22 +3,15 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 number = list(map(int, input().split()))
-s = [0] * n  # prefix_sum
-s[0] = number[0]
+prefix_sum = 0
 c = [0] * m  # same index count
+c[0] = 1
 count = 0  # 구간의 개수
 
-for i in range(1, n):
-    s[i] = s[i-1] + number[i]
-
 for i in range(n):
-    remainder = s[i] % m
-    if remainder == 0:
-        count += 1
+    prefix_sum += number[i]
+    remainder = prefix_sum % m
+
+    count += c[remainder]
     c[remainder] += 1
-
-for i in range(m):
-    if c[i] > 1:  # same index > 1
-        count += c[i] * (c[i] - 1) // 2
-
 print(count)
