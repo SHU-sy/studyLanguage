8584268a91a9f5dad7a62a2ff6918d7b8a757c3a
SHU <jisu.kimjss@gmail.com>
2024-10-10 13:21:21 +0900

    [Bronze II] Title: OX퀴즈, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Bronze/8958. OX퀴즈/OX퀴즈.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+testcase = int(input())
+for _ in range(testcase):
+    a = input().strip()
+    count = 0
+    score = 0
+    for i in a:
+        if i == "O":
+            count += 1
+            score += count
+        elif i == "X":
+            count = 0
+    print(score)
