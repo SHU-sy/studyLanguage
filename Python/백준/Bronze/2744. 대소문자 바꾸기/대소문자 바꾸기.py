8a5738e999b114aac677398993f397c2d1078ef4
SHU <jisu.kimjss@gmail.com>
2024-10-10 13:37:59 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ Python/백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,9 +1,1 @@
-import sys
-a = sys.stdin.readline().strip()
-r = []
-for i in range(len(a)):
-    if 65 <= ord(a[i]) <= 90:
-        r.append(a[i].lower())
-    else:
-        r.append(a[i].upper())
-print(''.join(r))
+print(input().swapcase())
