33f9d13110230a9e4cb06a6a862333d8267d8c49
SHU <jisu.kimjss@gmail.com>
2024-10-15 17:44:18 +0900

    [Bronze III] Title: ACM 호텔, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ Python/백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -3,7 +3,7 @@ input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
     h, w, n = map(int, input().split())
+    f = n%h
     if n % h == 0:
-        print(f"{h}{(n-1)//h+1:02}")
-        continue
-    print(f"{n % h}{(n-1)//h+1:02}")
+        f = h
+    print(f"{f}{(n-1)//h+1:02}")
