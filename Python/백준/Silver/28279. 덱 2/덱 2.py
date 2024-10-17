8240d8ff63ed2a7f418f931c51d1f4d21af99497
SHU <jisu.kimjss@gmail.com>
2024-10-17 23:30:15 +0900

    [Silver IV] Title: 덱 2, Time: 1148 ms, Memory: 70608 KB -BaekjoonHub

--- Python/백준/Silver/28279. 덱 2/덱 2.py
+++ Python/백준/Silver/28279. 덱 2/덱 2.py
@@ -5,34 +5,36 @@ n = int(input())
 ans = deque()
 for i in range(n):
     p = list(map(int, input().split()))
-    if p[0] == 1:
-        ans.appendleft(p[1])
-    if p[0] == 2:
-        ans.append(p[1])
-    if p[0] == 3:
-        if ans:
-            print(ans.popleft())
-        else:
-            print("-1")
-    if p[0] == 4:
-        if ans:
-            print(ans.pop())
-        else:
-            print("-1")
-    if p[0] == 5:
-        print(len(ans))
-    if p[0] == 6:
-        if ans:
-            print("0")
-        else:
-            print("1")
-    if p[0] == 7:
-        if ans:
-            print(ans[0])
-        else:
-            print("-1")
-    if p[0] == 8:
-        if ans:
-            print(ans[-1])
-        else:
-            print("-1")
+
+    match p[0]:
+        case 1:
+            ans.appendleft(p[1])
+        case 2:
+            ans.append(p[1])
+        case 3:
+            if ans:
+                print(ans.popleft())
+            else:
+                print("-1")
+        case 4:
+            if ans:
+                print(ans.pop())
+            else:
+                print("-1")
+        case 5:
+            print(len(ans))
+        case 6:
+            if ans:
+                print("0")
+            else:
+                print("1")
+        case 7:
+            if ans:
+                print(ans[0])
+            else:
+                print("-1")
+        case 8:
+            if ans:
+                print(ans[-1])
+            else:
+                print("-1")
