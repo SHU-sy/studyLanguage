36c56aea7efd11da02183f7b72f990c1f3ad1075
SHU <jisu.kimjss@gmail.com>
2024-10-19 23:52:54 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 1360 ms, Memory: 34052 KB -BaekjoonHub

--- Python/백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ Python/백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -2,35 +2,40 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
-belt = deque(map(int, input().split()))
-robot = deque([0] * n)
-step = 0
-zero_count = belt.count(0)
+def conveyor_belt(n, k, belt):
+    robot = deque([False] * n)
+    step = 0
+    zero_count = belt.count(0)
 
-while True:
-    step += 1
+    while True:
+        step += 1
 
-    belt.rotate(1)
-    robot.rotate(1)
-    robot[-1] = 0
+        belt.rotate(1)
+        robot.rotate(1)
+        robot[-1] = False
 
-    for i in range(n-2, -1, -1):
-        if robot[i] == 1 and robot[i+1] == 0 and belt[i+1] > 0:
-            robot[i] = 0
-            robot[i+1] = 1
-            belt[i+1] -= 1
-            if belt[i+1] == 0:
+        for i in range(n-2, -1, -1):
+            if robot[i] and robot[i+1] == 0 and belt[i+1] > 0:
+                robot[i] = False
+                robot[i+1] = True
+                belt[i+1] -= 1
+                if belt[i+1] == 0:
+                    zero_count += 1
+
+        robot[-1] = False
+
+        if belt[0] > 0 and not robot[0]:
+            robot[0] = True
+            belt[0] -= 1
+            if belt[0] == 0:
                 zero_count += 1
 
-    robot[-1] = 0
+        if zero_count >= k:
+            break
 
-    if belt[0] > 0:
-        robot[0] = 1
-        belt[0] -= 1
-        if belt[0] == 0:
-            zero_count += 1
+    return step
 
-    if zero_count >= k:
-        break
-print(step)
+n, k = map(int, input().split())
+belt = deque(map(int, input().split()))
+answer = conveyor_belt(n, k, belt)
+print(answer)
