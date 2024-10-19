db971caac70a1d1ee3cb7925740edfd0248368f7
SHU <jisu.kimjss@gmail.com>
2024-10-19 23:35:17 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2776 ms, Memory: 34072 KB -BaekjoonHub

--- Python/백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ Python/백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -6,6 +6,7 @@ n, k = map(int, input().split())
 belt = deque(map(int, input().split()))
 robot = deque([0] * n)
 step = 0
+zero_count = belt.count(0)
 
 while True:
     step += 1
@@ -14,18 +15,22 @@ while True:
     robot.rotate(1)
     robot[-1] = 0
 
-    for i in range(n-1, -1, -1):
+    for i in range(n-2, -1, -1):
         if robot[i] == 1 and robot[i+1] == 0 and belt[i+1] > 0:
             robot[i] = 0
             robot[i+1] = 1
             belt[i+1] -= 1
+            if belt[i+1] == 0:
+                zero_count += 1
 
     robot[-1] = 0
 
     if belt[0] > 0:
         robot[0] = 1
         belt[0] -= 1
+        if belt[0] == 0:
+            zero_count += 1
 
-    if belt.count(0) >= k:
+    if zero_count >= k:
         break
 print(step)
