3c62172ec44dcb5df9a177804c09828711bce6d5
SHU <jisu.kimjss@gmail.com>
2024-10-23 18:26:59 +0900

    [Silver IV] Title: 카드, Time: 124 ms, Memory: 43212 KB -BaekjoonHub

--- Python/백준/Silver/11652. 카드/카드.py
+++ Python/백준/Silver/11652. 카드/카드.py
@@ -6,6 +6,6 @@ num_dict = defaultdict(int)
 for _ in range(n):
     num_dict[int(input())] += 1
 
-ans = sorted(num_dict.items(), key=lambda item: (-item[1], item[0]))
-
-print(ans[0][0])
+num_max = max(num_dict.values())
+ans = [key for key, value in num_dict.items() if num_max == value]
+print(sorted(ans)[0])
