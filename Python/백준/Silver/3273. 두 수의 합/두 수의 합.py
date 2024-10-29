daaedcaabf0f1e16b23ddd7f923d52c979637660
SHU <jisu.kimjss@gmail.com>
2024-10-29 20:31:34 +0900

    [Silver III] Title: 두 수의 합, Time: 72 ms, Memory: 42036 KB -BaekjoonHub

--- Python/백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ Python/백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,23 +1,27 @@
 import sys
 input = sys.stdin.readline
 
+def count_pointer(n, arr, x):
+    arr.sort()
+    s = 0
+    e = len(arr) - 1
+    count = 0
+
+    while s < e:
+        current_sum = arr[s] + arr[e]
+        if current_sum == x:
+            count += 1
+            s += 1
+            e -= 1
+
+        elif current_sum < x:
+            s += 1
+
+        else:
+            e -= 1
+    return count
+
 n = int(input())
-arr = sorted(list(map(int, input().split())))
+arr = list(map(int, input().split()))
 x = int(input())
-s = 0
-e = len(arr)-1
-count = 0
-
-while s < e:
-    current_sum = arr[s] + arr[e]
-    if current_sum == x:
-        count += 1
-        s += 1
-        e -= 1
-
-    elif current_sum < x:
-        s += 1
-
-    else:
-        e -= 1
-print(count)
+print(count_pointer(n, arr, x))
