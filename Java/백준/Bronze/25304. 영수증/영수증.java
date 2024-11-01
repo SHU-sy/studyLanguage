bfe96f62366185c1337fe91921ae1ce80b51004b
SHU <jisu.kimjss@gmail.com>
2024-11-01 19:03:55 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int x = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int sum = 0;
+
+        for (int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sum += a*b;
+        }
+
+        bw.write(sum==x ? "Yes":"No");
+        bw.flush();
+    }
+}
