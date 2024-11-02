d3d27d88c78f962d8e3340937d69cd39f342d59c
SHU <jisu.kimjss@gmail.com>
2024-11-02 16:27:04 +0900

    [Bronze IV] Title: Hello Judge, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

---
+++ Java/백준/Bronze/9316. Hello Judge/Hello Judge.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i=1; i<=n; i++){
+            sb.append("Hello World, Judge ").append(i).append("!\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
