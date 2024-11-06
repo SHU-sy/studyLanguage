81c62333d770729ed391b80528e3c644bd9554dd
SHU <jisu.kimjss@gmail.com>
2024-11-06 18:27:44 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ Python/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,7 +1,3 @@
 import sys
-w = sys.stdin.readline().strip()
-
-if w[:len(w)] == w[-len(w):][::-1]:
-    print("1")
-else:
-    print("0")
+word = sys.stdin.readline().strip()
+print(1 if word==word[::-1] else 0)
