19423aa66fb76445f17ba64f4c40209ac6e07f46
SHU <jisu.kimjss@gmail.com>
2024-11-12 01:27:16 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 100 ms, Memory: 14136 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double a = Integer.parseInt(st.nextToken());
+        double b = Integer.parseInt(st.nextToken());
+
+        System.out.println(((a-a*b*0.01) < 100) ? 1 : 0);
+    }
+}
