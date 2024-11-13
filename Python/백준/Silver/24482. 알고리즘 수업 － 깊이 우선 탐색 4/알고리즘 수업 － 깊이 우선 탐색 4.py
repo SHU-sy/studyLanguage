64ef6648cd85f6d15c7636a8566c994433a38b35
SHU <jisu.kimjss@gmail.com>
2024-11-14 01:27:46 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 532 ms, Memory: 75836 KB -BaekjoonHub

---
+++ Python/백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(200000)
+
+def dfs (graph, node, visited, depth, current_d):
+    visited[node] = True
+    depth[node] = current_d
+
+    for neighbor in graph[node]:
+        if not visited[neighbor]:
+            dfs(graph, neighbor, visited, depth, current_d+1)
+
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(1, n+1):
+    graph[i].sort(reverse=True)
+
+visited = [False] * (n+1)
+depth = [-1] * (n + 1)
+
+dfs(graph, r, visited, depth, 0)
+
+print("\n".join(map(str, depth[1:])))
