57f263cc4e73d2a1e60c0ba5e2ea0e0dbe986098
SHU <jisu.kimjss@gmail.com>
2024-11-30 21:20:00 +0900

    [Silver V] Title: 이장님 초대, Time: 84 ms, Memory: 42036 KB -BaekjoonHub

---
+++ Python/백준/Silver/9237. 이장님 초대/이장님 초대.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tree = sorted(list(map(int , input().split())), reverse=True)
+
+for i in range(n):
+	tree[i] += i+1
+
+print(max(tree)+1)
