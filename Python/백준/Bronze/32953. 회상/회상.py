c287391d937c553aea1e2870223d79850b0e4d35
SHU <jisu.kimjss@gmail.com>
2025-01-14 20:22:57 +0900

    [Bronze I] Title: 회상, Time: 52 ms, Memory: 34012 KB -BaekjoonHub

--- Python/백준/Bronze/32953. 회상/회상.py
+++ Python/백준/Bronze/32953. 회상/회상.py
@@ -1,15 +1,17 @@
 import sys
-from collections import defaultdict
 input = sys.stdin.readline
 
-counter = defaultdict(int)
+counter = dict()
 
 n, m = map(int, input().split())
 for _ in range(n):
     k = int(input())
     temp = list(map(int, input().split()))
     for key in temp:
-        counter[key] += 1
+        if key in counter:
+            counter[key] += 1
+        else:
+            counter[key] = 1
 
 count = 0
 for key in counter:
