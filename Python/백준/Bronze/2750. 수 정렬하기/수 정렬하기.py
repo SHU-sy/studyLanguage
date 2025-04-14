06758ce8351d2bfabcb4460ccef4a4018f733461
SHU <jisu.kimjss@gmail.com>
2025-04-14 16:42:38 +0900

    [Bronze II] Title: 수 정렬하기, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
+++ Python/백준/Bronze/2750. 수 정렬하기/수 정렬하기.py
@@ -1,5 +1,6 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
-num = sorted(int(input().strip()) for _ in range(n))
-sys.stdout.write("\n".join(map(str, num)))
+num = sorted([int(input()) for _ in range(n)])
+print(*num, sep="\n")
