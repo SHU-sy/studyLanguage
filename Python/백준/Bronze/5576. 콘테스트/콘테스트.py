32ed0554e4dcceb73796c0638441573f19e21670
SHU <jisu.kimjss@gmail.com>
2025-04-14 16:53:43 +0900

    [Bronze II] Title: 콘테스트, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/5576. 콘테스트/콘테스트.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+w = sorted([int(input()) for _ in range(10)], reverse=True)
+k = sorted([int(input()) for _ in range(10)], reverse=True)
+print(sum(w[:3]), sum(k[:3]))
