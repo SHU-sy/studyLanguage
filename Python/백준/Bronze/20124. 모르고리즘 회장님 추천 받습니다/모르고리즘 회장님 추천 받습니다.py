d87f9cba23b4c2ed16f2ab7267c4846cac697579
SHU <jisu.kimjss@gmail.com>
2025-04-15 16:41:31 +0900

    [Bronze II] Title: 모르고리즘 회장님 추천 받습니다, Time: 152 ms, Memory: 60600 KB -BaekjoonHub

--- Python/백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.py
+++ Python/백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.py
@@ -2,6 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-member = [(input().split()) for _ in range(n)]
-ans = sorted(member, key=lambda x: (-int(x[1]), x[0]))
-print(ans[0][0])
+members = [(input().split()) for _ in range(n)]
+
+best = members[0]
+
+for member in members[1:]:
+    current_score = int(member[1])
+    best_score = int(best[1])
+    if best_score < current_score or (best_score==current_score and best[0] > member[0]):
+        best = member
+
+print(best[0])
