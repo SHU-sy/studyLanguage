e11c340ca5e21b75365ca9bcb1f2fd183d9a594f
SHU <jisu.kimjss@gmail.com>
2025-04-21 11:26:04 +0900

    [Silver IV] Title: 문자열 집합, Time: 84 ms, Memory: 37432 KB -BaekjoonHub

--- Python/백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ Python/백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -2,12 +2,5 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-s_set = set()
-for i in range(n):
-    s_set.add(input().strip())
-
-count = 0
-for i in range(m):
-    if input().strip() in s_set:
-        count += 1
-print(count)
+s = {input().strip() for _ in range(n)}
+print(sum(1 for _ in range(m) if input().strip() in s))
