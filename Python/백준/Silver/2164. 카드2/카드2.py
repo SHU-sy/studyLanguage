d7a7978f21f52445b2f3ac7dd6608d659ea66cf5
SHU <jisu.kimjss@gmail.com>
2025-04-30 12:13:30 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 55796 KB -BaekjoonHub

--- Python/백준/Silver/2164. 카드2/카드2.py
+++ Python/백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,13 @@
 import sys
 from collections import deque
-n = int(sys.stdin.readline().strip())
-card = deque([i+1 for i in range(n)])
-ans = []
-count = 1
+input = sys.stdin.readline
 
-while card:
-        ans.append(card.popleft())
-        card.rotate(-1)
-print(" ".join(map(str, ans[-1:])))
+n = int(input())
+cards = deque(list(range(1, n+1)))
+
+while True:
+    if len(cards) == 1:
+        print(cards[0])
+        break
+    cards.popleft()
+    cards.rotate(-1)
