6e8f0fca254c458a29208a31abb25a2138db0af5
SHU <jisu.kimjss@gmail.com>
2025-05-01 00:56:49 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 4732 ms, Memory: 138072 KB -BaekjoonHub

---
+++ Python/백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    cards = list(map(int, input().split()))
+    heapq.heapify(cards)
+
+    total = 0
+    while len(cards) > 1:
+        temp = heapq.heappop(cards) + heapq.heappop(cards)
+        heapq.heappush(cards, temp)
+        total += temp
+    print(total)
