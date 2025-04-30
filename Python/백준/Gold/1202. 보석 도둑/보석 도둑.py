75cead78cf4f811523ddde1918fb2c199e0868fc
SHU <jisu.kimjss@gmail.com>
2025-04-30 23:56:35 +0900

    [Gold II] Title: 보석 도둑, Time: 812 ms, Memory: 119544 KB -BaekjoonHub

--- Python/백준/Gold/1202. 보석 도둑/보석 도둑.py
+++ Python/백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -1,21 +1,32 @@
 import sys
 import heapq
+from collections import defaultdict
+from heapq import heappush, heappop
+
 input = sys.stdin.readline
 
 n, k = map(int, input().split())
-juwels = [list(map(int, input().split())) for _ in range(n)]
-juwels.sort(key=lambda x: x[0])
+juwels = defaultdict(list)
+for _ in range(n):
+    m, v = map(int, input().split())
+    juwels[m].append(v)
+
+mass_list = sorted(juwels.keys())
 
 bags = sorted([int(input()) for _ in range(k)])
 
 total = 0
 heap = []
 j_index = 0
+j_len = len(mass_list)
 
 for bag in bags:
-    while j_index < n and juwels[j_index][0] <= bag:
-        heapq.heappush(heap, -juwels[j_index][1])
+    while j_index < j_len and mass_list[j_index] <= bag:
+        for juwel in juwels[mass_list[j_index]]:
+            heappush(heap, -juwel)
         j_index += 1
+
     if heap:
-        total += -heapq.heappop(heap)
+        total += -heappop(heap)
+
 print(total)
