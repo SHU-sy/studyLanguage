a314887b138130307bd8450051308a2b5edadb47
SHU <jisu.kimjss@gmail.com>
2025-05-13 17:02:07 +0900

    [Gold V] Title: 소셜 네트워킹 어플리케이션, Time: 732 ms, Memory: 39800 KB -BaekjoonHub

--- Python/백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
+++ Python/백준/Gold/7511. 소셜 네트워킹 어플리케이션/소셜 네트워킹 어플리케이션.py
@@ -3,19 +3,20 @@ input = sys.stdin.readline
 
 t = int(input())
 
-def find(x):
+def find(x, parent):
     if parent[x] != x:
-        parent[x] = find(parent[x])
+        parent[x] = find(parent[x], parent)
     return parent[x]
 
-def union(a, b):
-    root_a = find(a)
-    root_b = find(b)
+def union(a, b, parent):
+    root_a = find(a, parent)
+    root_b = find(b, parent)
     if root_a != root_b:
         parent[root_b] = root_a
 
+output = []
 for i in range(1, t+1):
-    print(f"Scenario {i}:")
+    output.append(f"Scenario {i}:")
     n = int(input())
     k = int(input())
 
@@ -23,10 +24,11 @@ for i in range(1, t+1):
 
     for _ in range(k):
         a, b = map(int, input().split())
-        union(a, b)
+        union(a, b, parent)
 
     m = int(input())
     for _ in range(m):
         a, b = map(int, input().split())
-        print(0 if find(a)!= find(b) else 1)
-    print("")
+        output.append("0" if find(a, parent)!= find(b, parent) else "1")
+    output.append("")
+print('\n'.join(output))
