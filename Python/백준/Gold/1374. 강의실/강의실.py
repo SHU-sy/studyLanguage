c388973d9beb1a37578d0a668ab47440ba253d14
SHU <jisu.kimjss@gmail.com>
2025-05-14 16:29:04 +0900

    [Gold V] Title: 강의실, Time: 252 ms, Memory: 54004 KB -BaekjoonHub

--- Python/백준/Gold/1374. 강의실/강의실.py
+++ Python/백준/Gold/1374. 강의실/강의실.py
@@ -1,16 +1,17 @@
 import sys
 import heapq
 input = sys.stdin.readline
+
 n = int(input())
-count = 1
 lectures = sorted([tuple(map(int, input().split())) for _ in range(n)], key=lambda x: x[1])
-end_heap = [lectures[0][2]]
 
-for lecture in lectures[1:]:
-    if end_heap and lecture[1] >= end_heap[0]:
-        heapq.heappop(end_heap)
+end_times = []
 
-    heapq.heappush(end_heap, lecture[2])
+for lecture in lectures:
+    _, start, end = lecture
 
-    count = max(count, len(end_heap))
-print(count)
+    if end_times and end_times[0] <= start:
+        heapq.heappop(end_times)
+    heapq.heappush(end_times, end)
+
+print(len(end_times))
