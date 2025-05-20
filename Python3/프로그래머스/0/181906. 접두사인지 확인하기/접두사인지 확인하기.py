4ae1e8ce66d8dc577371dfccf884c73ee84d90d5
SHU <jisu.kimjss@gmail.com>
2025-05-20 18:26:50 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.00 ms, Memory: 9.11 MB -BaekjoonHub

--- Python3/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
+++ Python3/프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.py
@@ -1,5 +1,2 @@
 def solution(my_string, is_prefix):
-    for i in range(len(my_string)):
-        if my_string[0:i] == is_prefix:
-            return 1
-    return 0
+    return int(my_string.startswith(is_prefix))
