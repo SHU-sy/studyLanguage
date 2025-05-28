a94de63dc53ca142a631e0eee4934da9d86e839c
SHU <jisu.kimjss@gmail.com>
2025-05-28 10:25:17 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Gold/1043. 거짓말/거짓말.py
+++ Python/백준/Gold/1043. 거짓말/거짓말.py
@@ -1,19 +1,16 @@
 import sys
 sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
 
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
     return parent[x]
 
-
 def union(a, b):
     root_a = find(a)
     root_b = find(b)
     if root_a == root_b:
-        return False
-
+        return
     if rank[root_a] < rank[root_b]:
         parent[root_a] = root_b
     elif rank[root_a] > rank[root_b]:
@@ -21,35 +18,45 @@ def union(a, b):
     else:
         parent[root_b] = root_a
         rank[root_a] += 1
-    return True
 
 
-n, m = map(int, input().split())
-true_list = list(map(int, input().split()))
-true_num = true_list[0]
+data = sys.stdin.read().split()
+idx = 0
 
-parent = [i for i in range(n+1)]
-rank = [0] * (n+1)
-count = 0
-party_list = []
+n, m = int(data[idx]), int(data[idx+1])
+idx += 2
 
+true_num = int(data[idx])
+idx += 1
 if true_num == 0:
     print(m)
     sys.exit()
-else:
-    true_people = true_list[1:]
-    for i in range(1, true_num):
-        union(true_people[i], true_people[i - 1])
 
+true_people = list(map(int, data[idx:idx+true_num]))
+idx += true_num
+
+parent = [i for i in range(n+1)]
+rank = [0] * (n+1)
+
+for i in range(1, true_num):
+    union(true_people[i], true_people[i-1])
+
+party_list = []
 for _ in range(m):
-    party_num, *party_people = map(int, input().split())
+    party_size = int(data[idx])
+    idx += 1
+    party_people = list(map(int, data[idx:idx+party_size]))
+    idx += party_size
     party_list.append(party_people)
-    for i in range(1, party_num):
+    for i in range(1, party_size):
         union(party_people[i], party_people[i-1])
 
-true_root = set(find(p) for p in true_people)
+if true_people:
+    root_true = find(true_people[0])
+
+count = 0
 for party in party_list:
-    if any(find(person) in true_root for person in party):
-        continue
-    count += 1
+    if all(find(person) != root_true for person in party):
+        count += 1
+
 print(count)
